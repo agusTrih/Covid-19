2ddba3f86110d3efe4bc00dbbadc6b3d348223a7
agusTrih <ant3dong@gmail.com>
2020-07-26 00:09:57 +0700

    ambil api covid-19

---
+++ src/Pages/CovidIndo/CovidIndo.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { fetchDataIndonesia } from "../../redux/actions";
+
+function CovidIndo(props) {
+    useEffect(() => {
+        props.dispatch(fetchDataIndonesia());
+
+        // eslint-disable-next-line
+    }, []);
+
+    return (
+        <div>
+            <h1>Update Covid-19</h1>
+            {props.indonesia.Countries !== undefined &&
+                props.indonesia.Countries.map((item) => {
+                    return (
+                        <div key={item.CountryCode}>
+                            <h2>Negara: {item.Country}</h2>
+                            <p>
+                                <strong>Tanggal:</strong> {item.Date}
+                            </p>
+                            <p>
+                                <strong>Positif:</strong> {item.NewConfirmed}
+                            </p>
+                            <p>
+                                <strong>Meninggal:</strong>
+                            </p>
+                        </div>
+                    );
+                })}
+        </div>
+    );
+}
+
+const mapStateToProps = (state) => {
+    console.log(state.indonesia);
+    return {
+        indonesia: state.indonesia,
+    };
+};
+export default connect(mapStateToProps)(CovidIndo);
